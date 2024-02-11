1eb973451cd45d87fd077635ea90d18da1ae81a9
OnceWasBiLLa <billapapaji@gmail.com>
2024-02-11 02:54:33 -0800

    .

--- cogs/help.py
+++ cogs/help.py
@@ -14,9 +14,6 @@ class MenuView(discord.ui.View):
         self.cur = self.con.cursor()        
 
     @discord.ui.select(placeholder="Fluffy Dev", options=[
-        discord.SelectOption(label="Antinuke", value="antinuke"),
-        discord.SelectOption(label="Autoresponder", value="autoresponder"), 
-        discord.SelectOption(label="Moderation", value="moderation"), 
         discord.SelectOption(label="Utility", value="utility"),
         discord.SelectOption(label="Music", value="music"),
         discord.SelectOption(label="Filter", value="filter"),
@@ -28,19 +25,7 @@ class MenuView(discord.ui.View):
                 await interaction.response.send_message("Sorry Bro, This is not your interaction.", ephemeral=True)
                 return
             selected_values = select.values
-            if selected_values and "antinuke" in selected_values:
-                embed = discord.Embed(colour=0x2b2d31, description="`Antinuke Enable`, `Antinuke Disable`, `Extraowner Add`, `Extraowner Remove`, `Extraowner Config`, `Extraowner Clear`, `Whitelist Show`, `Whitelist Add`, `Whitelist Remove`, `Whitelist Clear`, `Logging Set`, `Logging Config`, `Logging Change`")
-                embed.set_author(name="Antinuke Commands", icon_url=Fluffy.icon)
-                await interaction.response.edit_message(embed=embed, view=self)
-            elif selected_values and "autoresponder" in selected_values:
-                embed = discord.Embed(colour=0x2b2d31, description="`Autoresponder`, `Autoresponder Create`, `Autoresponder Delete`, `Autoresponder Config`, `Autoresponder Edit`, `Autoresponder Clear`")
-                embed.set_author(name="Autoresponse Commands", icon_url=Fluffy.icon)
-                await interaction.response.edit_message(embed=embed, view=self)
-            elif selected_values and "moderation" in selected_values:
-                embed = discord.Embed(colour=0x2b2d31, description="`Kick`, `Ban`, `Unban`, `Mute`, `Unmute`, `Lock`, `Lockall`, `Unlock`, `Unlockall`, `Hide`, `Hideall`, `Unhide`, `Unhideall`, `Nuke`, `Purge`, `Purge Contains`, `Purge user`, `Purge startswith`, `Purge endswith`, `Purge invites`")
-                embed.set_author(name="Moderation Commands", icon_url=Fluffy.icon)
-                await interaction.response.edit_message(embed=embed, view=self)
-            elif selected_values and "utility" in selected_values:
+            if selected_values and "utility" in selected_values:
                 embed = discord.Embed(colour=0x2b2d31, description="`Avatar`, `Banner`, `MemberCount`")
                 embed.set_author(name="Utility Commands", icon_url=Fluffy.icon)
                 await interaction.response.edit_message(embed=embed, view=self)                
@@ -81,7 +66,7 @@ class Help(commands.Cog):
         server_prefix = cur.fetchone()
         prefix = server_prefix[0] if server_prefix else "&"
         view = MenuView(ctx.author)
-        embed = discord.Embed(colour=0x2b2d31, description=f"{dot} My prefix for this server is `{prefix}`\n{dot} Total Commands `70`\n{dot} [**Fluffy**]({Fluffy.bot_link}) | [**Support**]({Fluffy.support_link})\n{dot} Thanks for using Fluffy")
+        embed = discord.Embed(colour=0x2b2d31, description=f"{dot} My prefix for this server is `{prefix}`\n{dot} Total Commands {len(self.bot.commands)}\n{dot} [**Fluffy**]({Fluffy.bot_link}) | [**Support**]({Fluffy.support_link})\n{dot} Thanks for using Fluffy")
         embed.add_field(name="__Commands__", value=f"**{antinuke}`:`Antinuke\n{autoresp}`:`Autoresponder\n{moderation}`:`Moderation\n{utility}`:`Utility\n{music}`:`Music\n{filters}`:`Filters\n{info}`:`Info**")
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)

d05ad95c95030219c19a4e003f9eb0a6c8c07c10
YhWach.xD <146439498+ItsBablu@users.noreply.github.com>
2024-02-23 18:25:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 
 os.system("pip install wavelink==2.6.4")
 import wavelink
-os.system("pip install git+https://github.com/ItsBablu/FluffShaku.git")
 
 import jishaku
 from discord.ext import commands, tasks

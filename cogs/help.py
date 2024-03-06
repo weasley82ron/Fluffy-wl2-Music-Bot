778fa8a33452b480eb93d30f4d357ba15a30f1b1
YhWach.xD <146439498+ItsBablu@users.noreply.github.com>
2024-03-06 13:45:43 +0530

    Update help.py

--- cogs/help.py
+++ cogs/help.py
@@ -5,81 +5,60 @@ from discord.ext import commands
 import Fluffy
 from emojis import *
 
-
 class MenuView(discord.ui.View):
     def __init__(self, author, timeout=30):
         super().__init__(timeout=timeout)
         self.author = author
-        self.value = None
-        self.con = sqlite3.connect("database.db")
-        self.cur = self.con.cursor()
+        self.value = None  
+        self.con = sqlite3.connect('database.db')
+        self.cur = self.con.cursor()        
 
-    @discord.ui.select(
-        placeholder="Fluffy Dev",
-        options=[
-            discord.SelectOption(label="Music", value="music"),
-            discord.SelectOption(label="Utility", value="utility"),
-            discord.SelectOption(label="Filter", value="filter"),
-            discord.SelectOption(label="Info", value="info"),
-        ],
-    )
-    async def select_category(
-        self, interaction: discord.Interaction, select: discord.ui.Select
-    ):
+    @discord.ui.select(placeholder="Fluffy Dev", options=[
+        discord.SelectOption(label="Music", value="music"),
+        discord.SelectOption(label="Utility", value="utility"),
+        discord.SelectOption(label="Filter", value="filter"),
+        discord.SelectOption(label="Info", value="info"),
+        ])
+    async def select_category(self, interaction: discord.Interaction, select: discord.ui.Select):
         try:
             if interaction.user.id != self.author.id:
-                await interaction.response.send_message(
-                    "Sorry Bro, This is not your interaction.", ephemeral=True
-                )
+                await interaction.response.send_message("Sorry Bro, This is not your interaction.", ephemeral=True)
                 return
             selected_values = select.values
             if selected_values and "music" in selected_values:
-                embed = discord.Embed(
-                    colour=Fluffy.color,
-                    description="`Play`, `Pause`, `Resume`, `Stop`, `Queue`, `Volume`, `Skip`, `ClearQueue`, `DefaultVolume`, `Move`, `Join`, `Leave`, `NowPlaying`, `Forward`, `Rewind`, `Seek`, `Remove`",
-                )
+                embed = discord.Embed(colour=Fluffy.color, description="`Play`, `Pause`, `Resume`, `Stop`, `Queue`, `Volume`, `Skip`, `ClearQueue`, `DefaultVolume`, `Move`, `Join`, `Leave`, `NowPlaying`, `Forward`, `Rewind`, `Seek`, `Remove`")
                 embed.set_author(name="Music Commands", icon_url=Fluffy.icon)
                 await interaction.response.edit_message(embed=embed, view=self)
             elif selected_values and "utility" in selected_values:
-                embed = discord.Embed(
-                    colour=Fluffy.color,
-                    description="`Avatar`, `Banner`, `MemberCount`, `Afk`",
-                )
+                embed = discord.Embed(colour=Fluffy.color, description="`Avatar`, `Banner`, `MemberCount`, `Afk`")
                 embed.set_author(name="Utility Commands", icon_url=Fluffy.icon)
-                await interaction.response.edit_message(embed=embed, view=self)
+                await interaction.response.edit_message(embed=embed, view=self)                
             elif selected_values and "filter" in selected_values:
-                embed = discord.Embed(
-                    colour=Fluffy.color,
-                    description="`Vaporwave`, `Lofi`, `8d`, `Slowmo`, `BassBoost`, `China`, `Chipmunk`, `DarthVader`, `Demon`, `Funny`, `Karoke`, `NightCore`, `Pop`, `Soft`, `TrebleBass`, `Tremolo`, `Alien`, `Reset`",
-                )
+                embed = discord.Embed(colour=Fluffy.color, description="`Vaporwave`, `Lofi`, `8d`, `Slowmo`, `BassBoost`, `China`, `Chipmunk`, `DarthVader`, `Demon`, `Funny`, `Karoke`, `NightCore`, `Pop`, `Soft`, `TrebleBass`, `Tremolo`, `Alien`, `Reset`")
                 embed.set_author(name="Filter Commands", icon_url=Fluffy.icon)
                 await interaction.response.edit_message(embed=embed, view=self)
             elif selected_values and "info" in selected_values:
-                embed = discord.Embed(
-                    colour=Fluffy.color,
-                    description="`Ping`, `Purge`, `Uptime`, `Invite`, `Support`, `Stats`, `Setpreifx`, `Help`",
-                )
+                embed = discord.Embed(colour=Fluffy.color, description="`Ping`, `Purge`, `Uptime`, `Invite`, `Support`, `Stats`, `Setpreifx`, `Help`")
                 embed.set_author(name="Info Commands", icon_url=Fluffy.icon)
                 await interaction.response.edit_message(embed=embed, view=self)
-
-            select.placeholder = None
+            
+            select.placeholder = None 
         except Exception as e:
             print(f"An error occurred: {e}")
-            raise
-
-
+            raise   
+            
 class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.client.remove_command("help")
-        self.con = sqlite3.connect("database.db")
-        self.cur = self.con.cursor()
+        self.con = sqlite3.connect('database.db')
+        self.cur = self.con.cursor()          
 
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Help Is Ready")
+        print("Help Is Ready")    
 
-    @commands.command(aliases=["h"])
+    @commands.command(aliases=['h'])
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def help(self, ctx, *, query=None):
         cur = self.con.cursor()
@@ -87,51 +66,28 @@ class Help(commands.Cog):
         server_prefix = cur.fetchone()
         prefix = server_prefix[0] if server_prefix else "&"
         view = MenuView(ctx.author)
-        embed = discord.Embed(
-            colour=Fluffy.color,
-            description=f"{dot} My prefix for this server is `{prefix}`\n{dot} Total Commands `45`\n{dot} [**Fluffy**]({Fluffy.bot_link}) | [**Support**]({Fluffy.support_link})\n{dot} Thanks for using Fluffy",
-        )
-
-        embed.add_field(
-            name="Music",
-            value="`Play`, `Pause`, `Resume`, `Stop`, `Queue`, `Volume`, `Skip`, `ClearQueue`, `DefaultVolume`, `Move`, `Join`, `Leave`, `NowPlaying`, `Forward`, `Rewind`, `Seek`, `Remove`",
-            inline=False,
-        )
-        embed.add_field(
-            name="Filters",
-            value="`Vaporwave`, `Lofi`, `8d`, `Slowmo`, `BassBoost`, `China`, `Chipmunk`, `DarthVader`, `Demon`, `Funny`, `Karaoke`, `NightCore`, `Pop`, `Soft`, `TrebleBass`, `Tremolo`, `Alien`, `Reset`",
-            inline=False,
-        )
-        embed.add_field(
-            name="Utility",
-            value="`Avatar`, `Banner`, `MemberCount`, `Afk`",
-            inline=False,
-        )
-        embed.add_field(
-            name="Info",
-            value="`Ping`, `Purge`, `Uptime`, `Invite`, `Support`, `Stats`, `Setprefix`, `Help`",
-            inline=False,
-        )
-
+        embed = discord.Embed(colour=Fluffy.color, description=f"{dot} My prefix for this server is `{prefix}`\n{dot} Total Commands `45`\n{dot} [**Fluffy**]({Fluffy.bot_link}) | [**Support**]({Fluffy.support_link})\n{dot} Thanks for using Fluffy")
+        
+        embed.add_field(name="Music", value='`Play`, `Pause`, `Resume`, `Stop`, `Queue`, `Volume`, `Skip`, `ClearQueue`, `DefaultVolume`, `Move`, `Join`, `Leave`, `NowPlaying`, `Forward`, `Rewind`, `Seek`, `Remove`', inline=False)
+        embed.add_field(name="Filters", value='`Vaporwave`, `Lofi`, `8d`, `Slowmo`, `BassBoost`, `China`, `Chipmunk`, `DarthVader`, `Demon`, `Funny`, `Karaoke`, `NightCore`, `Pop`, `Soft`, `TrebleBass`, `Tremolo`, `Alien`, `Reset`', inline=False)
+        embed.add_field(name="Utility", value='`Avatar`, `Banner`, `MemberCount`, `Afk`', inline=False)
+        embed.add_field(name="Info", value='`Ping`, `Purge`, `Uptime`, `Invite`, `Support`, `Stats`, `Setpreifx`, `Help`', inline=False)
+        
         embed.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
         embed.set_thumbnail(url=ctx.author.display_avatar.url)
         embed.set_footer(text="By Fluffy Services", icon_url=Fluffy.icon)
-
+        
         if query:
             command = self.client.get_command(query)
             if command:
                 aliases = ", ".join(command.aliases)
-                ryze = discord.Embed(
-                    color=Fluffy.color, description=f"**{command.help}**"
-                )
-                ryze.add_field(name="Aliases", value=f"`{aliases}`", inline=False)
-                ryze.add_field(name="Usage", value=f"`{command.usage}`", inline=False)
-                ryze.set_author(
-                    name=ctx.author.name, icon_url=ctx.author.display_avatar.url
-                )
-                ryze.set_thumbnail(url=ctx.author.display_avatar.url)
-                ryze.set_footer(text="By Fluffy Services", icon_url=Fluffy.icon)
-                await ctx.send(embed=ryze)
+                bablu=discord.Embed(color=Fluffy.color, description=f"**{command.help}**")
+                bablu.add_field(name="Aliases", value=f"`{aliases}`", inline=False)
+                bablu.add_field(name="Usage", value=f"`{command.usage}`", inline=False)
+                bablu.set_author(name=ctx.author.name, icon_url=ctx.author.display_avatar.url)
+                bablu.set_thumbnail(url=ctx.author.display_avatar.url)
+                bablu.set_footer(text="By Fluffy Services", icon_url=Fluffy.icon)
+                await ctx.send(embed=bablu)
                 return
             else:
                 await ctx.send("Command not found.")
